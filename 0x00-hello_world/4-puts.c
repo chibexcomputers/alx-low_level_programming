42845eb173703cb024f9e0d543967a02285dc8c5
chibexcomputers <chibexcomputers@gmail.com>
2022-10-13 15:26:46 -0700

    task c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/** main - Prints "Programming is like building a multilingual puzzle, 
- *  
- * Return: Always 0.
+/** main - Prints "Programming is like building a multilingual puzzle,
+ *
+ * RETURN: Always 0.
  */
 
 int main(void)
