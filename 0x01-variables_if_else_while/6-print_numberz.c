08bfbb0651468536452e0c56ef2072661ac68d73
chibexcomputers <chibexcomputers@gmail.com>
2022-10-17 08:17:10 -0700

    task variable

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
- *
- *  * main - entry point
- *
- *   * Return: Always 0
- *
- *    */
+* main - entry point
+*Return: Always 0
+*/
 int main(void)
 
 {
-
-		int i;
-
-
-
-			for (i = '0'; i <= '9'; i++)
-
-					putchar(i);
-
-				putchar('\n');
-
-					return (0);
-
-						}
+int i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
