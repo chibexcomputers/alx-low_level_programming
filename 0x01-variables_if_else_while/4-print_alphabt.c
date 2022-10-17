75868a84f08c5220710dc3f08fc8457ab036157f
chibexcomputers <chibexcomputers@gmail.com>
2022-10-17 08:03:32 -0700

    task variable

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,11 @@
-/*
-** File: 4-print_alphabt.c
-** Auth: Brennan D Baraban
-
-*/
-
 #include <stdio.h>
 
 /**
-** main - Prints the alphabet in lowercase, except for q and e.
-** Return: Always 0.
+* main - Prints the alphabet in lowercase, except for q and e.
+* Return: Always 0.
+*/
 
-**/
-
-int main(void);
+int main(void)
 
 {
 
