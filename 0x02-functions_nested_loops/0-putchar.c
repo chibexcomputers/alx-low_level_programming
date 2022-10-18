f5a948669e5b0cd06edf830a7a0805d3ec7c49b7
chibexcomputers <chibexcomputers@gmail.com>
2022-10-18 08:16:43 -0700

    nested func

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-#include <unistd.h>
-
-
+#include "holberton.h"
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* main - Entry point
+* Return: Always 0 (Success)
 */
 
-
-
-int _putchar(char c)
-
-
+int main(void)
 
 {
-
-
-return (write(1, &c, 1));
-
-
+char *sh = "Holberton";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+putchar('\n');
+return (0);
 
 }
