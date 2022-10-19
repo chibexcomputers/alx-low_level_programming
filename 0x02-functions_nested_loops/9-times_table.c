61263f6c3c9f421781d970080978e8c08790ff03
chibexcomputers <chibexcomputers@gmail.com>
2022-10-18 20:22:38 -0700

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,42 @@
-#include "main.h"
+#include "holberton.h"
+
+
 
 /**
-* times_table - a function that prints the 9 times table, starting with 0.
-* Return: Nothing
+* times_table - Prints the 9 times table, starting with 0
+*Return: empty output
 */
 
 void times_table(void)
 
 {
-int row, column, product;
-for (row = 0; row <= 9; row++)
+int x, y, z, u, d;
+for (x = 0; x <= 9; x++)
 {
-_putchar(0);
-for (column = 0; column <= 9; column++)
+for (y = 0; y <= 9; y++)
 {
-_putchar(' ');
-_putchar(',');
-product = row * column;
-if (product <= 9)
-_putchar(' ');
+z = x * y;
+if (z > 9)
+{
+u = z % 10;
+d = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
 else
-_putchar((product / 10) + '0');
-_putchar((product % 10) + '0');
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
 }
 _putchar('\n');
 }
 }
+
