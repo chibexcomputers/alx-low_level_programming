4111d16931bf7ed76e3d5bbbd932fd0343cc909f
chibexcomputers <chibexcomputers@gmail.com>
2022-10-18 19:59:11 -0700

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ int row, column, product;
 for (row = 0; row <= 9; row++)
 {
 _putchar(0);
-for (column = 1; column <= 9; column++)
+for (column = 0; column <= 9; column++)
 {
 _putchar(',');
 _putchar(' ');
