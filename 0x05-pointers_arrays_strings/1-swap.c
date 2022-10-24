99dc4368bdc78de2aca9f1d2f22293fe7afc5b3e
chibexcomputers <chibexcomputers@gmail.com>
2022-10-24 08:23:57 -0700

    reset

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include < stdio.h >
+#include <stdio.h>
 
 
 
 /**
 * main - check the code
-*
-* Return: Always 0.
+*Return: Always 0.
 */
 
 int main(void)
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -16,7 +16,7 @@ void puts2(char *str)
 
 {
 int a = 0, b = 0;
-hile (str[a++])
+while (str[a++])
 b++;
 for (a = 0; a < b; a += 2)
 _putchar(str[a]);
