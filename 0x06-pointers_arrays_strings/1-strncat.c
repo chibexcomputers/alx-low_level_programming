d981d10f1d504dbba9b0f5c3e521996ffb2adb5f
chibexcomputers <chibexcomputers@gmail.com>
2022-10-27 06:21:43 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+
+/**
+* _strncat - a function that concatenates two strings
+* @dest:copy to
+* @src: copy from
+* @n: n
+* Return: Always 0 (Success)
+*/
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+int i, co;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (co = 0; co < n; co++)
+{
+dest[i + co] = src[co];
+if (src[co] == '\0')
+co = n;
+}
+return (dest);
+}
+
+Footer
+
+© 2022 GitHub, Inc.
