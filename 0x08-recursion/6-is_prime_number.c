927cf79d0db246129a4d6ad9f9bd39d6f6581624
chibexcomputers <chibexcomputers@gmail.com>
2022-11-02 13:43:29 -0700

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,6 +19,7 @@ return (l - 1);
 return (palind2(a + 1, l + 1));
 }
 
+
 /**
 * palind3 - compares string vs string reverse
 * @a: string
@@ -27,14 +28,17 @@ return (palind2(a + 1, l + 1));
 * On error, -1 is returned, and errno is set appropriately.
 */
 
+
 int palind3(char *a, int l)
 
 {
+
 if (*a != *(a + l))
 return (0);
 else if (*a == 0)
 return (1);
 return (palind3(a + 1, l - 2));
+
 }
 
 /**
@@ -47,6 +51,7 @@ return (palind3(a + 1, l - 2));
 int is_palindrome(char *s)
 
 {
+
 int l;
 l = palind2(s, 0);
 return (palind3(s, l));
